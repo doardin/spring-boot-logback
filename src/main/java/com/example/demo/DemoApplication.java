7b7e4f5cca5838b9490587f03b82b0462a669ac2
Eduardo Lopes <duh.lopes234@hotmail.com>
2023-04-17 22:18:38 -0300

    feat: Integration with Logback

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,40 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public void teste() throws Exception {
+        try {
+            method1();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void method1() throws Exception {
+        try {
+            method2();
+        } catch (Exception e) {
+            throw new Exception("Error in method1", e);
+        }
+    }
+
+    public void method2() throws Exception {
+        try {
+            int result = 1 / 0; // This will throw an ArithmeticException
+        } catch (Exception e) {
+            throw new Exception("Error in method2", e);
+        }
+    }
 
 }
